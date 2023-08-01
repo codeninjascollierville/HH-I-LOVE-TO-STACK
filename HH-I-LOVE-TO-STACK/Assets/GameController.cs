9de42d626c50627adbde5b33286789a8ae24ba5c
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-08-01 18:16:32 -0500

    8/1/23

--- HH-I-LOVE-TO-STACK/Assets/GameController.cs
+++ HH-I-LOVE-TO-STACK/Assets/GameController.cs
@@ -1,9 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagment;
 public class GameController : MonoBehaviour
 {
+
+[Header("Cube Object")]
+public GameObject currentCube;
+
+
+[Header("Last Cube Object")]
+public GameObject lastCube;
+
+
+[Header("Text Object")]
+public Text text;
+
+[Header("Current Level")]
+public int Level
+
+[Header("Boolean")]
+public bool Done;
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +39,38 @@ public class GameController : MonoBehaviour
     {
         
     }
+
+
+void Newblock()
+{
+If(lastCube != null)
+{
+
+currentCube.transform.position = new Vector3
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
 }
